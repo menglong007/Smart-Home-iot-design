cd9264822092b41714635692f48ee8e148107539
Your Name <you@example.com>
2023-11-01 13:58:33 +0700

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyAlJC6xifQCweuwq_x4khfvaVTPPjL5jck",
-    authDomain: "angular-project-a47fc.firebaseapp.com",
-    databaseURL: "https://angular-project-a47fc-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "angular-project-a47fc",
-    storageBucket: "angular-project-a47fc.appspot.com",
-    messagingSenderId: "212609984046",
-    appId: "1:212609984046:web:4ab303e58f32fed079a1bf"
+    apiKey: "AIzaSyAU3Vi3e8cVRfRvRI3RFnZ4dQHL27N-eT0",
+    authDomain: "angular-project-a759f.firebaseapp.com",
+    databaseURL: "https://angular-project-a759f-default-rtdb.firebaseio.com",
+    projectId: "angular-project-a759f",
+    storageBucket: "angular-project-a759f.appspot.com",
+    messagingSenderId: "325460200011",
+    appId: "1:325460200011:web:0d738eab077e927bacdbce"
   },
   production : false
 };
